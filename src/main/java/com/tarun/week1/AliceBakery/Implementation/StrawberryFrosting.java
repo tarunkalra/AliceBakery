a07131d5428797f432bfdc7b88e159f16a446534
Tarun Kalra <ktarun213@gmail.com>
2024-11-20 22:53:20 +0530

    Add matchIfMissing when property is not defined

--- src/main/java/com/tarun/week1/AliceBakery/Implementation/StrawberryFrosting.java
+++ src/main/java/com/tarun/week1/AliceBakery/Implementation/StrawberryFrosting.java
@@ -2,10 +2,11 @@ package com.tarun.week1.AliceBakery.Implementation;
 
 import com.tarun.week1.AliceBakery.interfaces.Frosting;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(name = "cake.frosting", havingValue = "strawberry")
+@ConditionalOnProperty(name = "cake.frosting", havingValue = "strawberry", matchIfMissing = true)
 public class StrawberryFrosting implements Frosting {
     public String getFrostType() {
         return "Strawberry frosting";
--- src/main/java/com/tarun/week1/AliceBakery/Implementation/StrawberrySyrup.java
+++ src/main/java/com/tarun/week1/AliceBakery/Implementation/StrawberrySyrup.java
@@ -2,10 +2,11 @@ package com.tarun.week1.AliceBakery.Implementation;
 
 import com.tarun.week1.AliceBakery.interfaces.Syrup;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(name = "cake.syrup", havingValue = "strawberry")
+@ConditionalOnProperty(name = "cake.syrup", havingValue = "strawberry", matchIfMissing = true)
 public class StrawberrySyrup implements Syrup {
     public String getSyruptype() {
         return "Strawberry syrup";
